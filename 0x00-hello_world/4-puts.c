c1ff74a571fb814ddb2db82b8edf1d0592025168
Your Name <ronkeashaju@gmail.com>
2022-10-13 05:58:51 -0700

    all tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: int 0 if successfull
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
