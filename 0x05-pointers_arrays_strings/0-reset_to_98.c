11dddd237a67a39ac1b42e76eca054f010762682
Ronke Ashaju <ronkeashaju@gmail.com>
2022-10-24 16:33:05 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - to take a pointer and update it to 98
+ * @n : a pointer
+ * Return : void, Basically nothing is returned
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
