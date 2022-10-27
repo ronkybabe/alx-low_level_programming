d02091a049d1078fee53cb5f03b36c6897b66b5d
Ronke Ashaju <ronkeashaju@gmail.com>
2022-10-27 20:16:47 +0000

    task 8. rot13

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,10 +2,9 @@
 
 /**
  * cap_string - to upper case
- * @n: param
- * Return: char
+ * @str: string
+ * Return: pointer to string
  */
-
 char *cap_string(char *str)
 {
 	int i = 0;
@@ -15,10 +14,10 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (str[i - 1] == '' ||
-		    str[i -1] == '\t'||
-		    str[i - 1] == '\n ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n  ||
 		    str[i - 1] == ',' ||
-		    str[i - 1] == ';' ||
+		    str[i - 1] ==  ';'||
 		    str[i - 1] == '.' ||
 		    str[i - 1] == '!' ||
 		    str[i - 1] == '?' ||
@@ -27,7 +26,7 @@ char *cap_string(char *str)
 		    str[i - 1] == ')' ||
 		    str[i - 1] == '{' ||
 		    str[i - 1] == '}' ||
-		    i ==0)
+		    i == 0)
 			str[i] -= 32;
 
 		i++;
