976bdbaac4e5a9f36241c058b13a01544c1e9411
Ronke Ashaju <ronkeashaju@gmail.com>
2023-05-11 12:54:23 +0000

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+ * linear_search - search linear form in array for element
+ * @array: array list
+ * @size: size of the array
+ * @value: value of the array
+ *
+ * Return: index where user looks for
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	for (i = 0; (i < size) && (array); i++)
+	{
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", (int)i, *(array + i));
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%lu] = [%d]\n", (int)i, *(array + i));
+		}
+	}
+	return (-1);
+}
