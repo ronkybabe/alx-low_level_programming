b271dc20249a9d21b16d67f6aa2fc0b690cdd29a
Ronke Ashaju <ronkeashaju@gmail.com>
2023-05-12 10:03:16 +0000

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i;
+	int i;
 
-        for (i = 0; i < size; i++)
-        {
-                if(array[i] == value)
-                        return(i);
-        }
+	if (array == NULL)
+		return (-1);
 
-        return (-1);
-        }
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
